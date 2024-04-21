d80127928111d6642742c3b5c748ef7005d2ec59
srirakeshv <srirakeshv@gmail.com>
2024-04-21 17:42:55 +0530

    uploaded files

--- script1.js
+++ script1.js
@@ -1,35 +1,40 @@
 // thirukural api
-let first = document.querySelector('#firstline');
-let second = document.querySelector('#secondline');
-let meaning=document.querySelector('#explanation')
+let first = document.querySelector("#firstline");
+let second = document.querySelector("#secondline");
+let meaning = document.querySelector("#explanation");
 let currentKuralNumber = 1;
 let lastUpdateTimestamp = 0;
 
 function fetchAndUpdateThirukural(kuralNumber) {
-    fetch(`https://api-thirukkural.vercel.app/api?num=${kuralNumber}`)
-    .then(res => res.json())
-    .then(msg => {
-        let line1 = msg.line1;
-        let line2 = msg.line2;
-        let explain=msg.tam_exp;
-        first.innerHTML = '"' + line1;
-        second.innerHTML = line2 + '"';
-        meaning.innerHTML=explain
+  fetch(`thirukkural.json`)
+    .then((res) => {
+      return res.json();
     })
-    .catch(error => {
-        console.error('Error fetching Thirukural:', error);
+    .then((msg) => {
+      console.log(msg);
+      let selectedKural = msg.kural[kuralNumber - 1];
+      console.log(selectedKural);
+      let line1 = selectedKural.Line1;
+      let line2 = selectedKural.Line2;
+      let explain = selectedKural.mk;
+      first.innerHTML = '"' + line1;
+      second.innerHTML = line2 + '"';
+      meaning.innerHTML = explain;
+    })
+    .catch((error) => {
+      console.error("Error fetching Thirukural:", error);
     });
 }
 
 function updateThirukuralOncePerDay() {
-    const now = Date.now();
-    const oneDayInMillis = 24 * 60 * 60 * 1000; // One day in milliseconds
+  const now = Date.now();
+  const oneDayInMillis = 24 * 60 * 60 * 1000; // One day in milliseconds
 
-    if (now - lastUpdateTimestamp >= oneDayInMillis) {
-        fetchAndUpdateThirukural(currentKuralNumber);
-        currentKuralNumber = (currentKuralNumber % 1330) + 1;
-        lastUpdateTimestamp = now;
-    }
+  if (now - lastUpdateTimestamp >= oneDayInMillis) {
+    fetchAndUpdateThirukural(currentKuralNumber);
+    currentKuralNumber = (currentKuralNumber % 1330) + 1;
+    lastUpdateTimestamp = now;
+  }
 }
 
 // Fetch and update immediately when the page loads
